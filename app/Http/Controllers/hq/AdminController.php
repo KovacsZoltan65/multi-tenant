85a5ce20b6ea7c9e4631b4110e9c00d275969fb7
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2025-05-25 18:50:35 +0200

    hq - email lekérés

--- app/Http/Controllers/hq/AdminController.php
+++ app/Http/Controllers/hq/AdminController.php
@@ -3,12 +3,55 @@
 namespace App\Http\Controllers\hq;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Spatie\Multitenancy\Models\Tenant;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        dd('Admin felület');
+        $tenants = Tenant::where('name', '<>', 'HQ')->get()->toArray();
+        echo '<pre>';
+        //print_r($tenants);
+        
+        foreach( $tenants as $tenant ) {
+          
+            print_r($tenant['name'] . '<br />');
+            
+            $connectionName = 'tenant_' . $tenant['id'];
+                    
+            Config::set("database.connections.{$connectionName}", [
+                'driver' => 'mysql',
+                'host' => $tenant['host'],
+                'port' => $tenant['port'],
+                'database' => $tenant['database'],
+                'username' => $tenant['username'],
+                'password' => $tenant['password'],
+                'charset' => 'utf8mb3',
+                'collation' => 'utf8mb3_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => 'InnoDB',
+            ]);
+            
+            DB::purge($connectionName); // Előző kapcsolat törlése, ha volt
+            DB::reconnect($connectionName); // Új kapcsolat létrehozása
+            
+            //$emails = DB::connection($connectionName)->table('emails')->get();
+            // vagy
+            $emails = \App\Models\Email::on($connectionName)->get();
+            
+            foreach($emails as $email) {
+                print_r($email->to . '<br />');
+            }
+            print_r('----------------------' . '<br />');
+
+            // További feldolgozás...
+            
+        }
+        
+        echo '</pre>';
     }
 }
